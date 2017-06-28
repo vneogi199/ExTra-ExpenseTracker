6f102e33406257b5c8ea7ddfb557a9cd1517bdb1
Vinit Neogi <vneogi199@gmail.com>
2017-06-28 17:53:24 +0530

    Initial commit

---
+++ app/src/main/java/comexpensetracker/medium/extra_expensetracker/AddExpense.kt
@@ -0,0 +1,12 @@
+package comexpensetracker.medium.extra_expensetracker
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AddExpense : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_expense)
+    }
+}
