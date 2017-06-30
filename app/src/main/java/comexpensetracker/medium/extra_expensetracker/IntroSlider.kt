8e1d08d197f95fb2013859219a058d0b2d7db271
Akash Dabhi <akashdabhi03@yahoo.com>
2017-07-01 01:18:06 +0530

    Intro Slider added.

---
+++ app/src/main/java/comexpensetracker/medium/extra_expensetracker/IntroSlider.kt
@@ -0,0 +1,12 @@
+package comexpensetracker.medium.extra_expensetracker
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class IntroSlider : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro_slider)
+    }
+}
