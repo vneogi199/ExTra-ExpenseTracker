44f4716843247191e53326abaaf3d8d67e6ea287
Akash Dabhi <akashdabhi03@yahoo.com>
2017-07-29 23:49:33 +0530

    Started validation of login page

--- app/src/main/java/comexpensetracker/medium/extra_expensetracker/LoginForm.kt
+++ app/src/main/java/comexpensetracker/medium/extra_expensetracker/LoginForm.kt
@@ -1,11 +1,16 @@
 package comexpensetracker.medium.extra_expensetracker
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.TextInputLayout
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import comexpensetracker.medium.extra_expensetracker.R.id.input_layout_username
+import comexpensetracker.medium.extra_expensetracker.R.id.input_username
 import io.hasura.sdk.Hasura
 import io.hasura.sdk.exception.HasuraException
 import io.hasura.sdk.HasuraUser
@@ -34,9 +39,12 @@ class LoginForm : BaseActivity() {
             e.printStackTrace()
         }
 
+        var inputLayoutUsername = findViewById(R.id.input_layout_username) as TextInputLayout
+        var inputLayoutPassword = findViewById(R.id.input_layout_password) as TextInputLayout
+
         val client = Hasura.getClient()
-        var user:HasuraUser = client.user
-        val btnLogin : Button = findViewById(R.id.btn_login) as Button
+        var user: HasuraUser = client.user
+        val btnLogin: Button = findViewById(R.id.btn_login) as Button
         btnLogin.setOnClickListener({
             var input_username = findViewById(R.id.input_username) as EditText
             var inputPassword = findViewById(R.id.input_password) as EditText
@@ -55,4 +63,46 @@ class LoginForm : BaseActivity() {
             })
         })
     }
+
+    fun View.showKeyboard() {
+        this.requestFocus()
+        val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+    }
+
+    private void submitForm()
+    {
+        if (!validateUsername()) {
+            return
+        }
+
+        if (!validatePassword()) {
+            return
+        }
+    }
+
+    private fun validateUsername(): Boolean {
+        if (input_username.toString().trim().isEmpty()) {
+            input_layout_username.setError(getString(R.string.err_msg_name))
+            showKeyboard(input_username)
+            return false
+        } else {
+            input_layout_username.setErrorEnabled(false)
+        }
+
+        return true
+    }
+
+    private fun validatePassword(): Boolean {
+        if (inputPassword.getText().toString().trim().isEmpty()) {
+            inputLayoutPassword.setError(getString(R.string.err_msg_password))
+            requestFocus(inputPassword)
+            return false
+        } else {
+            inputLayoutPassword.setErrorEnabled(false)
+        }
+
+        return true
+    }
+
 }
