c76c2dcdb04187ce7e94e384b843b2170ad1e71b
Vinit Neogi <vneogi199@gmail.com>
2017-08-12 16:04:53 +0530

    Inserted values into graph

--- app/src/main/java/comexpensetracker/medium/extra_expensetracker/ViewAnalytics.kt
+++ app/src/main/java/comexpensetracker/medium/extra_expensetracker/ViewAnalytics.kt
@@ -6,12 +6,19 @@ import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.Toast
 import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
 import io.hasura.sdk.*
 import io.hasura.sdk.exception.HasuraException
 import io.hasura.sdk.exception.HasuraInitException
 import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
+import com.github.mikephil.charting.formatter.PercentFormatter
+import android.R.attr.data
+import android.graphics.Color
+
 
 /**
  * Created by vinit on 10/8/17.
@@ -37,14 +44,25 @@ class ViewAnalytics : AppCompatActivity() {
         user = client?.user
 
         showGraph()
-
-
     }
 
     fun showGraph() {
         var pieChart: PieChart = findViewById(R.id.piechart) as PieChart
         pieChart.setUsePercentValues(true)
-        var expenseArray = IntArray(9)
+        var totalArray = IntArray(9)
+        for(i in 0..8) totalArray[i] = 0
+        var expenseArray:ArrayList<Entry> = ArrayList()
+        var xVals:ArrayList<String> = ArrayList()
+        xVals.add("Bills")
+        xVals.add("Groceries")
+        xVals.add("Entertainment")
+        xVals.add("Fuel")
+        xVals.add("Food")
+        xVals.add("Health")
+        xVals.add("Travel")
+        xVals.add("Shopping")
+        xVals.add("Other")
+
         try {
 //            val jsonObject = JSONObject("  {\"type\":\"select\"," +
 //                    " \"args\":{" +
@@ -76,21 +94,31 @@ class ViewAnalytics : AppCompatActivity() {
                             for (record in response) {
                                 Log.i("ResponseRecord", record.getExpCategory().toString())
                                 when(record.getExpCategory()){
-                                    "Bills" -> expenseArray[0] = expenseArray[0] + record.getExpAmt()
-                                    "Groceries" -> expenseArray[1] = expenseArray[1] + record.getExpAmt()
-                                    "Entertainment" -> expenseArray[2] = expenseArray.get(2) + record.getExpAmt()
-                                    "Fuel" -> expenseArray[3] = expenseArray[3] + record.getExpAmt()
-                                    "Food" -> expenseArray[4] = expenseArray[4] + record.getExpAmt()
-                                    "Health" -> expenseArray[5] = expenseArray[5] + record.getExpAmt()
-                                    "Travel" -> expenseArray[6] = expenseArray[6] + record.getExpAmt()
-                                    "Shopping" -> expenseArray[7] = expenseArray[7] + record.getExpAmt()
-                                    "Other" -> expenseArray[8] = expenseArray[8] + record.getExpAmt()
+                                    "Bills" -> totalArray[0] = totalArray[0] + record.getExpAmt()
+                                    "Groceries" -> totalArray[1] = totalArray[1] + record.getExpAmt()
+                                    "Entertainment" -> totalArray[2] = totalArray[2] + record.getExpAmt()
+                                    "Fuel" -> totalArray[3] = totalArray[3] + record.getExpAmt()
+                                    "Food" -> totalArray[4] = totalArray[4] + record.getExpAmt()
+                                    "Health" -> totalArray[5] = totalArray[5] + record.getExpAmt()
+                                    "Travel" -> totalArray[6] = totalArray[6] + record.getExpAmt()
+                                    "Shopping" -> totalArray[7] = totalArray[7] + record.getExpAmt()
+                                    "Other" -> totalArray[8] = totalArray[8] + record.getExpAmt()
                                     else -> Toast.makeText(this@ViewAnalytics, "Error while calculating", Toast.LENGTH_SHORT).show()
                                 }
                             }
-                            for(element in expenseArray){
-                                Log.d("ExpenseArray", element.toString())
+                            for(i in 0..8){
+                                expenseArray.add(Entry(totalArray[i].toFloat(), i))
                             }
+
+                            var data_set:PieDataSet = PieDataSet(expenseArray, "Expense Analytics")
+                            var data:PieData = PieData(xVals, data_set)
+                            data.setValueFormatter(PercentFormatter())
+                            pieChart.data = data
+                            pieChart.setDescription("")
+                            pieChart.isDrawHoleEnabled = false
+                            data.setValueTextSize(33f)
+                            data.setValueTextColor(Color.DKGRAY)
+                            Log.d("EXPENSE ARRAY", expenseArray.toString())
                         }
 
                         override fun onFailure(e: HasuraException) {
