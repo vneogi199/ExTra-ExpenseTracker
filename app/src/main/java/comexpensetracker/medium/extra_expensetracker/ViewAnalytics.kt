7b5129f801b8146d4b7f050afe12dc8b44c92652
Vinit Neogi <vneogi199@gmail.com>
2017-08-17 00:36:21 +0530

    Added dropdown on ViewAnalytics page and created template for bar graph

--- app/src/main/java/comexpensetracker/medium/extra_expensetracker/ViewAnalytics.kt
+++ app/src/main/java/comexpensetracker/medium/extra_expensetracker/ViewAnalytics.kt
@@ -21,8 +21,8 @@ import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
 import android.widget.ArrayAdapter
-
-
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.data.BarDataSet
 
 
 /**
@@ -30,8 +30,8 @@ import android.widget.ArrayAdapter
  */
 class ViewAnalytics : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
+
     var spinner : Spinner? = null
-    var options = arrayOf("Graph", "Pie Chart")
     var user: HasuraUser? = null
     var client: HasuraClient? = null
 
@@ -52,15 +52,13 @@ class ViewAnalytics : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         user = client?.user
 
         spinner = findViewById(R.id.spinner) as Spinner?
-        val adapter = ArrayAdapter<String>(this@ViewAnalytics,
-                R.layout.activity_view_expense, options)
-        //adapter.setDropDownViewResource()
-        //TODO()
-
-        showGraph()
+        val adapter = ArrayAdapter.createFromResource(this, R.array.dropdown_list, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner?.adapter = adapter
+        spinner?.onItemSelectedListener = this
     }
 
-    fun showGraph() {
+    fun showPieChart() {
         var pieChart: PieChart = findViewById(R.id.piechart) as PieChart
         pieChart.setUsePercentValues(true)
         var totalArray = IntArray(9)
@@ -139,8 +137,7 @@ class ViewAnalytics : AppCompatActivity(), AdapterView.OnItemSelectedListener {
                             var legend:Legend = pieChart.legend
                             legend.isEnabled = false
                             data.setValueTextSize(15f)
-
-                            Log.d("EXPENSE ARRAY", expenseArray.toString())
+                            pieChart.invalidate()
                         }
 
                         override fun onFailure(e: HasuraException) {
@@ -154,12 +151,108 @@ class ViewAnalytics : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             Toast.makeText(this@ViewAnalytics, e.toString(), Toast.LENGTH_SHORT).show()
         }
     }
+
+    fun showGraph() {
+        var barChart: BarChart = findViewById(R.id.barchart) as BarChart
+        var totalArray = IntArray(9)
+        for(i in 0..8) totalArray[i] = 0
+        var expenseArray : ArrayList<Entry> = ArrayList()
+        var categoryValues : ArrayList<String> = ArrayList()
+        categoryValues.add("Bills")
+        categoryValues.add("Groceries")
+        categoryValues.add("Entertainment")
+        categoryValues.add("Fuel")
+        categoryValues.add("Food")
+        categoryValues.add("Health")
+        categoryValues.add("Travel")
+        categoryValues.add("Shopping")
+        categoryValues.add("Other")
+
+        var xVals : ArrayList<String> = ArrayList()
+        try {
+//            val jsonObject = JSONObject("  {\"type\":\"select\"," +
+//                    " \"args\":{" +
+//                    "\"table\":\"expense_info_view\", \"columns"\: ["id", "title"],
+//                    "}" +
+//                    "}")
+
+            var columnsArray = JSONArray()
+            columnsArray.put("exp_amt")
+            columnsArray.put("category_name")
+
+            var args = JSONObject()
+            args.put("table", "expense_info_view")
+            args.put("columns", columnsArray)
+
+            var userIDQuery = JSONObject()
+            userIDQuery.put("user_id", user?.id)
+
+            args.put("where", userIDQuery)
+
+            var selectExpenseQuery = JSONObject()
+            selectExpenseQuery.put("type", "select")
+            selectExpenseQuery.put("args", args)
+            Log.i("ResponseRecord", selectExpenseQuery.toString())
+            client?.useDataService()?.setRequestBody(selectExpenseQuery)
+                    ?.expectResponseTypeArrayOf(ExpenseRecord::class.java)
+                    ?.enqueue(object : Callback<List<ExpenseRecord>, HasuraException> {
+                        override fun onSuccess(response: List<ExpenseRecord>) {
+                            for (record in response) {
+                                Log.i("ResponseRecord", record.getExpCategory().toString())
+                                when(record.getExpCategory()){
+                                    "Bills" -> totalArray[0] = totalArray[0] + record.getExpAmt()
+                                    "Groceries" -> totalArray[1] = totalArray[1] + record.getExpAmt()
+                                    "Entertainment" -> totalArray[2] = totalArray[2] + record.getExpAmt()
+                                    "Fuel" -> totalArray[3] = totalArray[3] + record.getExpAmt()
+                                    "Food" -> totalArray[4] = totalArray[4] + record.getExpAmt()
+                                    "Health" -> totalArray[5] = totalArray[5] + record.getExpAmt()
+                                    "Travel" -> totalArray[6] = totalArray[6] + record.getExpAmt()
+                                    "Shopping" -> totalArray[7] = totalArray[7] + record.getExpAmt()
+                                    "Other" -> totalArray[8] = totalArray[8] + record.getExpAmt()
+                                    else -> Toast.makeText(this@ViewAnalytics, "Error while calculating", Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                            for(i in 0..8){
+                                if(totalArray[i] != 0){
+                                    expenseArray.add(Entry(totalArray[i].toFloat(), i))
+                                    xVals.add(categoryValues[i])
+                                }
+
+                            }
+
+                            var bardataset: BarDataSet = BarDataSet(entries, "Categories")
+                            var data:PieData = PieData(xVals, data_set)
+                            data.setValueFormatter(PercentFormatter())
+                            pieChart.data = data
+                            pieChart.setDescription("")
+                            pieChart.isDrawHoleEnabled = false
+                            data_set.setColors(ColorTemplate.JOYFUL_COLORS)
+                            var legend:Legend = pieChart.legend
+                            legend.isEnabled = false
+                            data.setValueTextSize(15f)
+                            pieChart.invalidate()
+                        }
+
+                        override fun onFailure(e: HasuraException) {
+//                            hideProgressIndicator()
+//                            handleError(e)
+                            Toast.makeText(this@ViewAnalytics, e.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    })
+
+        } catch (e: JSONException) {
+            Toast.makeText(this@ViewAnalytics, e.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onNothingSelected(parent: AdapterView<*>?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if(position == 0){}
+        else{
+            showPieChart()
+        }
     }
 
 }
